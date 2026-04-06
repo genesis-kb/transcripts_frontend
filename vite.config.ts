adf3aca374d20252848a202aabce2b5d82c31209
staru09 <arusharmazxx000@gmail.com>
2026-04-06 17:52:01 +0530

    added workflow for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: mode === "production" ? "/transcripts_frontend/" : "/",
   server: {
     host: "::",
     port: 8080,
